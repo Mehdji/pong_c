e72eae6711f0d4ad2f758a582d9506584075b747
Mehdi <naouimehdi@hotmail.fr>
2025-10-30 02:24:13 +0100

    Refactor player rectangle handling and add upward/downward movement functionality

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ MEHDI NAOUI
 Pong
 SDL2 https://www.libsdl.org/
 Learning to code a pong game using YT video published by Daniel HIRSCH
+https://youtu.be/m_jDyqcvaQU?si=yy_pF8O90e-_n6XL
 */
 
 //include SDL libray wich allow to draw things on a window.
@@ -22,17 +23,84 @@ int main(int argc, char *argv[]) {
     SDL_Surface *surface = SDL_GetWindowSurface(window);
 
     //Create a rectangle using a compound syntax and store it in a SDL_Rect type variable.
-    SDL_Rect rectangle = (SDL_Rect) {40,40,40,40};
+    SDL_Rect rect_player1 = (SDL_Rect) {40,40,40,200};
     
     //White color stored in color.
     Uint32 color = 0xffffffff;
 
     //Fill the surface with the rectangle and color given in arguments.
-    SDL_FillRect(surface,&rectangle,color);
+    SDL_FillRect(surface,&rect_player1,color);
+
+    //Reclangle for second player:
+
+    SDL_Rect rect_player2 = (SDL_Rect) {560,150,40,200};
+   
+    SDL_FillRect(surface,&rect_player2,color);
 
     //Update the window
     SDL_UpdateWindowSurface(window);
     
-    SDL_Delay(10000);
+    
+
+    int running = 1;
+    SDL_Event event ;
+    SDL_KeyboardEvent keydown;
+
+    while (running){
+
+        SDL_PollEvent(&event);
+        if(event.type == SDL_QUIT){
+            running = 0;
+        }
+
+            if(event.type == SDL_KEYDOWN){
+                //printf("Key down!\n");
+
+                if (event.key.keysym.sym == SDLK_DOWN){
+                
+
+                //Fill old rectangle in black.
+                SDL_FillRect(surface,&rect_player1,0x0000000);
+
+                //Change rectangle position
+                rect_player1.y += 10; 
+
+                //Print coordinates in the console for debugging
+                printf("x_pl1: %d y_pl1: %d\n",rect_player1.x,rect_player1.y);
+
+                //Re-draw the rectangle  
+                SDL_FillRect(surface,&rect_player1,color);
+            
+                }
+
+
+            
+
+            
+                if (event.key.keysym.sym == SDLK_UP ){
+                
+
+                //Fill old rectangle in black.
+                SDL_FillRect(surface,&rect_player1,0x00000000);
+
+                //Change rectangle position
+                rect_player1.y -= 10; 
+
+                //Print coordinates in the console for debugging
+                printf("x_pl1: %d y_pl1: %d\n",rect_player1.x,rect_player1.y);
+
+                //Re-draw the rectangle  
+                SDL_FillRect(surface,&rect_player1,color);
+                
+            }
+
+        }
+
+        
+    //Refresh the window and redraws elements
+    SDL_UpdateWindowSurface(window);
+    }
+
+    SDL_Delay(50);
     return 0;
 }
